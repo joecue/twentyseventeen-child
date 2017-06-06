d789a175452ad2fe6feaa23d1974f48060f20be4
Joe Querin <joecue@gmail.com>
2017-06-06 17:50:48 -0400

    Updated Functions file

--- functions.php
+++ functions.php
@@ -1,5 +1,10 @@
 <?php
 
+function t7c_theme_enqueue_styles() {
+   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css' );
+}
 
+add_action( 'wp_enqueue_scripts', 't7c_theme_enqueue_styles' );
 
 ?>
